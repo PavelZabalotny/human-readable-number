32e6e182bb7dec0cf23cc21717ff18bc8fd6ffde
PavelZabalotny <pavelzabalotny@gmail.com>
2021-12-29 19:14:38 +0300

    feat: implement function toreabable

--- src/index.js
+++ src/index.js
@@ -1,3 +1,91 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(num) {
+    const arrayOfNum = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+
+    const arrayOfNumWithTwoChar = [
+        0,
+        1,
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+
+    const len = num.toString().length;
+    /**
+     * 1-19
+     */
+    if (num < 20) {
+        return arrayOfNum[num];
+    }
+    /**
+     * 20, 30, 40 ... 90
+     */
+    if (len === 2 && num % 10 === 0) {
+        return arrayOfNumWithTwoChar[num.toString()[0]];
+    }
+    /**
+     * 100, 200, 300 ... 900
+     */
+    if (len === 3 && num % 10 === 0 && num % 100 === 0) {
+        return `${arrayOfNum[num.toString()[0]]} hundred`;
+    }
+    /**
+     * 21, 22, 23, ... 99
+     */
+    if (len === 2) {
+        return `${arrayOfNumWithTwoChar[num.toString()[0]]} ${
+            arrayOfNum[num.toString()[1]]
+        }`;
+    }
+    /**
+     * 101, 102, 103 ... 119
+     */
+    if (len === 3 && +num.toString().slice(1) < 20) {
+        return `${arrayOfNum[num.toString()[0]]} hundred ${
+            +num.toString().slice(1) < 10
+                ? arrayOfNum[num.toString().slice(2)]
+                : arrayOfNum[num.toString().slice(1)]
+        }`;
+    }
+    /**
+     * 121, 122, 123 ... 999
+     */
+    if (len === 3) {
+        return `${
+            +num.toString()[2] === 0
+                ? arrayOfNum[num.toString()[0]] +
+                  " hundred " +
+                  arrayOfNumWithTwoChar[num.toString()[1]]
+                : arrayOfNum[num.toString()[0]] +
+                  " hundred " +
+                  arrayOfNumWithTwoChar[num.toString()[1]] +
+                  " " +
+                  arrayOfNum[num.toString()[2]]
+        }`;
+    }
+};
