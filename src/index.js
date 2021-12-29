e7e92f0db1713cd8a90914a279aea0d48c5dcd32
PavelZabalotny <pavelzabalotny@gmail.com>
2021-12-29 19:25:17 +0300

    refactor: conditional grouping

--- src/index.js
+++ src/index.js
@@ -43,39 +43,39 @@ module.exports = function toReadable(num) {
         return arrayOfNum[num];
     }
     /**
-     * 20, 30, 40 ... 90
-     */
-    if (len === 2 && num % 10 === 0) {
-        return arrayOfNumWithTwoChar[num.toString()[0]];
-    }
-    /**
-     * 100, 200, 300 ... 900
-     */
-    if (len === 3 && num % 10 === 0 && num % 100 === 0) {
-        return `${arrayOfNum[num.toString()[0]]} hundred`;
-    }
-    /**
      * 21, 22, 23, ... 99
      */
     if (len === 2) {
+        /**
+         * 20, 30, 40 ... 90
+         */
+        if (len === 2 && num % 10 === 0) {
+            return arrayOfNumWithTwoChar[num.toString()[0]];
+        }
         return `${arrayOfNumWithTwoChar[num.toString()[0]]} ${
             arrayOfNum[num.toString()[1]]
-        }`;
-    }
-    /**
-     * 101, 102, 103 ... 119
-     */
-    if (len === 3 && +num.toString().slice(1) < 20) {
-        return `${arrayOfNum[num.toString()[0]]} hundred ${
-            +num.toString().slice(1) < 10
-                ? arrayOfNum[num.toString().slice(2)]
-                : arrayOfNum[num.toString().slice(1)]
         }`;
     }
     /**
      * 121, 122, 123 ... 999
      */
     if (len === 3) {
+        /**
+         * 100, 200, 300 ... 900
+         */
+        if (len === 3 && num % 10 === 0 && num % 100 === 0) {
+            return `${arrayOfNum[num.toString()[0]]} hundred`;
+        }
+        /**
+         * 101, 102, 103 ... 119
+         */
+        if (len === 3 && +num.toString().slice(1) < 20) {
+            return `${arrayOfNum[num.toString()[0]]} hundred ${
+                +num.toString().slice(1) < 10
+                    ? arrayOfNum[num.toString().slice(2)]
+                    : arrayOfNum[num.toString().slice(1)]
+            }`;
+        }
         return `${
             +num.toString()[2] === 0
                 ? arrayOfNum[num.toString()[0]] +
